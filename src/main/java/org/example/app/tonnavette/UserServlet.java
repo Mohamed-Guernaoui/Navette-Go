efc2743f3aa0a27cb7034f9a3b04df6c7c7c03cb
Mohamed <looklike882@gmail.com>
2025-03-18 23:41:21 +0000

    [Add]: Sign in endpoint & api

---
+++ src/main/java/org/example/app/tonnavette/UserServlet.java
@@ -0,0 +1,34 @@
+package org.example.app.tonnavette;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.example.app.tonnavette.dao.UtilisateurDAO;
+import org.example.app.tonnavette.model.Utilisateur;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+@WebServlet(name= "signIn", value = "/sign-in")
+public class UserServlet extends HttpServlet {
+     protected void doPost(HttpServletRequest request, HttpServletResponse response)
+             throws ServletException, IOException {
+         Connection con = DatabaseConnection.getConnection();
+         UtilisateurDAO userDAO = new UtilisateurDAO(con);
+         String email = request.getParameter("email");
+         String password = request.getParameter("password");
+         Utilisateur user = userDAO.authentifier(email, password);
+         if (user != null) {
+             HttpSession session = request.getSession();
+             session.setAttribute("userAuth", user);
+             response.sendRedirect("index.jsp"); // Redirection après connexion
+         } else {
+             response.sendRedirect("views/login.jsp?error=Identifiants incorrects");
+         }
+
+
+     }
+}
