a22183eba71259ef0ced8801c889fb4911b3c2bc
Mohamed <looklike882@gmail.com>
2025-04-06 17:59:55 +0100

    [Add]: some extra att

--- src/main/java/org/example/app/tonnavette/model/Utilisateur.java
+++ src/main/java/org/example/app/tonnavette/model/Utilisateur.java
@@ -6,14 +6,18 @@ public class Utilisateur {
     private String email;
     private String motDePasse;
     private String role; // "Client" ou "Société"
+    private String telephone;
+    private String prenom;
 
     public Utilisateur(
-            int id, String nom, String email, String role
+            int id, String nom, String email, String role, String telephone, String prenom
     ) {
         this.id = id;
         this.nom = nom;
         this.email = email;
         this.role = role;
+        this.telephone = telephone;
+        this.prenom = prenom;
     }
 
     public Utilisateur(String nom, String email, String motDePasse, String role) {
@@ -30,28 +34,50 @@ public class Utilisateur {
     }
     public String getNom() {
         return nom;
-    };
+    }
+
     public void setNom(String nom) {
         this.nom = nom;
-    };
+    }
+
     public String getEmail() {
         return email;
-    };
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
     public String getMotDePasse() {
         return motDePasse;
-    };
+    }
+
     public void setMotDePasse(String motDePasse) {
         this.motDePasse = motDePasse;
-    };
+    }
+
     public String getRole() {
         return role;
-    };
+    }
+
     public void setRole(String role) {
         this.role = role;
-    };
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 
 
 
