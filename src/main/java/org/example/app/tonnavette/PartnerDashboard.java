cbc5929f123bbd618e12a987a67fdd6032e88b39
Mohamed <looklike882@gmail.com>
2025-04-29 22:33:13 +0100

    [Add]: Partner Dashboard Controller

---
+++ src/main/java/org/example/app/tonnavette/PartnerDashboard.java
@@ -0,0 +1,50 @@
+package org.example.app.tonnavette;
+
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.app.tonnavette.dao.EntrepriseDAO;
+import org.example.app.tonnavette.model.Entreprise;
+import org.example.app.tonnavette.model.Navette;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/partner-dashboard")
+public class PartnerDashboard extends HttpServlet {
+    // In PartnerDashboardServlet.java
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Entreprise partner = ((Entreprise) request.getSession().getAttribute("loggedPartner"));
+        if (
+                partner == null
+        ) {
+            response.sendRedirect("./views/business-account-login.jsp?action=sign-in&error=True");
+        }
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            System.out.println(partner.getId());
+            EntrepriseDAO dao = new EntrepriseDAO(connection);
+            List<Navette> navettes = dao.getNavettesByPartner(partner.getId());
+//
+//
+//        // Load all dashboard data
+            request.setAttribute("shuttles", navettes);
+////        request.setAttribute("revenue", dao.getRevenueStats(partner, "monthly"));
+////        request.setAttribute("utilization", dao.getShuttleUtilization(partner));
+            request.getRequestDispatcher("./views/partner-dashboard.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
